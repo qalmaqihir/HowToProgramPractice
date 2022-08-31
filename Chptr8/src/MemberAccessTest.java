9792d6f2a592b199e4742e1c0e59ec14885169f8
qalmaqihir <supine.me@gmail.com>
2022-08-31 10:44:11 +0600

    Access Members test

--- Chptr8/src/MemberAccessTest.java
+++ Chptr8/src/MemberAccessTest.java
@@ -1,6 +1,7 @@
 public class MemberAccessTest {
     public static void main(String[] args) {
         Time1 time = new Time1();
+        System.out.println("Testing access");
 //        time.hour=8;
 //        time.minute=50;
 //        time.second=34;
